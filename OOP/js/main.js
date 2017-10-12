550c583ba6141d23898e529e9dd3dfb912fb02e3
Henry <acarat@qq.com>
2017-10-12 13:30:40 +0800

    oop work online v0.0.1

---
+++ OOP/js/main.js
@@ -0,0 +1,10 @@
+// main.js
+/**
+ * config layui
+ */
+layui.use(['layer', 'element'], function() {
+	var layer = layui.layer
+	,element = layui.element;
+	
+	layer.msg("Welcome work Site...");
+});
